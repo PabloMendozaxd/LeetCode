3fb22edf18441dfa7bb322d157103eee694fce10
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-09-22 13:18:28 -0500

    Refactored and tested

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const CONSECUTIVE= array =>  {
+function countOnes(array){
     let lastVal;
     let contador=0;
     array.forEach(element => {
         if (lastVal!==undefined) {
-            if ((lastVal===1&&element===1)&&(lastVal===element)) {
-                contador++;
-            } else {
-                contador=0;
-            }
+            ((lastVal===1&&element===1)&&(lastVal===element))?contador++:contador=0;
         } else {
             lastVal=element;
         }
     });
-    console.log(contador)
+    return contador;
 }
 
-CONSECUTIVE([1,0,1,1,1,1,0,0,0,0,1,1]);
+module.exports=countOnes;
--- index.test.js
+++ index.test.js
@@ -1,1 +1,5 @@
+const index=require('./index');
 
+test('should count the max amount of consecutive number ones in the array', () => {
+    expect(index([1,1,0,0,0,1,1,1,1])).toBe(4);
+})
